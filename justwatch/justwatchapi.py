4fe26e2ba9461a99b5987b6270d1dcf3dd769c95
vonmaster <43321841+vonmaster@users.noreply.github.com>
2020-09-17 15:47:02 +0200

    Update justwatchapi.py
    Increased timeout variable

--- justwatch/justwatchapi.py
+++ justwatch/justwatchapi.py
@@ -9,7 +9,7 @@ HEADER = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/68.0.3440.84 Safari/537.36'
 }
-TIMEOUT = 5
+TIMEOUT = 15
 
 
 def create_session():
